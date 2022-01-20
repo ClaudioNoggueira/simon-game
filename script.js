99237a2d50e3f2a3e96582fd44bbbd528bbd1811
ClaudioNoggueira <cllaudionoggueira@gmail.com>
2022-01-20 12:59:29 -0300

    Adicionando sons aos botoes quando clicados

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var userClickedPattern = [];
 $(".btn").click(function () {
     var userChosenColour = $(this).attr("id");
     userClickedPattern.push(userChosenColour);
-    console.log(userClickedPattern);
+    playSound(userChosenColour);
 });
 
 function nextSequence() {
@@ -15,7 +15,11 @@ function nextSequence() {
     gamePattern.push(randomChosenColour);
 
     $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-    var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+    playSound(randomChosenColour);
+}
+
+function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
     audio.play();
 }
 
